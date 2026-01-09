ff089718696ae81895bc01884acb4124331f2844
javakishore-veleti <javakishore@gmail.com>
2026-01-09 06:38:38 -0500

    Initial version of auth-profiles back office implementation

--- microservices/customer-onboarding-service/customer-api/src/main/java/com/healthcare/customer/api/CustomerApiApplication.java
+++ microservices/customer-onboarding-service/customer-api/src/main/java/com/healthcare/customer/api/CustomerApiApplication.java
@@ -5,11 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.healthcare.customer")
-@EntityScan(basePackages = "com.healthcare.customer.common.model")
-@EnableJpaRepositories(basePackages = "com.healthcare.customer.dao.repository")
+@SpringBootApplication(scanBasePackages = {
+    "com.healthcare.customer"
+})
+@EntityScan(basePackages = {
+    "com.healthcare.customer.dao.entity",
+    "com.healthcare.customer.common.model"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.healthcare.customer.dao.repository"
+})
 public class CustomerApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CustomerApiApplication.class, args);
     }
